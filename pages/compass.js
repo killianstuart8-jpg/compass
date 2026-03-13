8bc2d223e943180524101b22b138ea2f8978490d
killianstuart8-jpg <killianstuart8@gmail.com>
2026-03-13 21:08:12 +0000

    Fix duplicate getServerSideProps function

--- pages/compass.js
+++ pages/compass.js
@@ -116,3 +116,6 @@ export default async function handler(req, res) {
 export async function getServerSideProps() {
   return { props: {} };
 }}
+export async function getServerSideProps() {
+  return { props: {} };
+}
